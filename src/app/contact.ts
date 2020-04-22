1638bcbfe2591c33875ef63b7b1a62cebbe0b20f
rajesh002 <rajeshbtech16@gmail.com>
2020-04-22 20:32:32 +0530

    complete

--- src/app/contact.ts
+++ src/app/contact.ts
@@ -1,139 +1,136 @@
-import { userContact } from './userContact';
+import { userContact } from "./userContact";
 
 export const contacts: userContact[] = [
   {
-    name: 'BlaisePascal',
-    email: 'Ashley_Ritchie@hotmail.com',
-    phoneNumber: '965-144-6018',
-    image: 'http://lorempixel.com/640/480/sports'
+    name: "BlaisePascal",
+    email: "Ashley_Ritchie@hotmail.com",
+    phoneNumber: "965-144-6018",
+    image: "http://lorempixel.com/640/480/sports",
   },
 
   {
-    name: 'AdaLovelace',
-    email: 'Rosendo53@hotmail.com',
-    phoneNumber: '1-522-714-2638 x44811',
-    image: 'http://lorempixel.com/640/480/food'
+    name: "AdaLovelace",
+    email: "Rosendo53@hotmail.com",
+    phoneNumber: "1-522-714-2638 x44811",
+    image: "http://lorempixel.com/640/480/food",
   },
 
   {
-    name: 'KurtGoedel',
-    email: 'Yolanda.Abernathy@hotmail.com',
-    phoneNumber: '(038) 030-9641 x7086',
-    image: 'http://lorempixel.com/640/480/nightlife'
+    name: "KurtGoedel",
+    email: "Yolanda.Abernathy@hotmail.com",
+    phoneNumber: "(038) 030-9641 x7086",
+    image: "http://lorempixel.com/640/480/nightlife",
   },
 
   {
-    name: 'Sachin Tendulkar',
-    email: 'Icie_Gutmann69@yahoo.com',
-    phoneNumber: '(251) 436-6001',
-    image: 'http://lorempixel.com/640/480/nightlife'
+    name: "Sachin Tendulkar",
+    email: "Icie_Gutmann69@yahoo.com",
+    phoneNumber: "(251) 436-6001",
+    image: "http://lorempixel.com/640/480/nightlife",
   },
 
   {
-    name: 'Alan Turing',
-    email: 'Brice46@yahoo.com',
-    phoneNumber: '005-851-7095 x72027',
-    image: 'http://lorempixel.com/640/480/food'
+    name: "Alan Turing",
+    email: "Brice46@yahoo.com",
+    phoneNumber: "005-851-7095 x72027",
+    image: "http://lorempixel.com/640/480/food",
   },
 
   {
-    name: 'A K Erlang',
-    email: 'Remington.Boyer63@yahoo.com',
-    phoneNumber: '1-471-836-5871 x5841',
-    image: 'http://lorempixel.com/640/480/technics'
+    name: "A K Erlang",
+    email: "Remington.Boyer63@yahoo.com",
+    phoneNumber: "1-471-836-5871 x5841",
+    image: "http://lorempixel.com/640/480/technics",
   },
 
   {
-    name: 'Niklaus Wirth',
-    email: 'Chasity.Metz83@gmail.com',
-    phoneNumber: '(247) 305-7427',
-    image: 'http://lorempixel.com/640/480/cats'
+    name: "Niklaus Wirth",
+    email: "Chasity.Metz83@gmail.com",
+    phoneNumber: "(247) 305-7427",
+    image: "http://lorempixel.com/640/480/cats",
   },
 
   {
-    name: 'Greg Whitten',
-    email: 'Izabella.Hand21@gmail.com',
-    phoneNumber: '475.071.4608 x101',
-    image: 'http://lorempixel.com/640/480/transport'
+    name: "Greg Whitten",
+    email: "Izabella.Hand21@gmail.com",
+    phoneNumber: "475.071.4608 x101",
+    image: "http://lorempixel.com/640/480/transport",
   },
 
   {
-    name: 'Reginald Kessler',
-    email: 'Retta.Wolf@gmail.com',
-    phoneNumber: '070-610-2661',
-    image: 'http://lorempixel.com/640/480/people'
+    name: "Reginald Kessler",
+    email: "Retta.Wolf@gmail.com",
+    phoneNumber: "070-610-2661",
+    image: "http://lorempixel.com/640/480/people",
   },
 
   {
-    name: 'Rosina Schiller',
-    email: 'Rachelle13@hotmail.com',
-    phoneNumber: '869.014.6216 x103',
-    image: 'http://lorempixel.com/640/480/transport'
+    name: "Rosina Schiller",
+    email: "Rachelle13@hotmail.com",
+    phoneNumber: "869.014.6216 x103",
+    image: "http://lorempixel.com/640/480/transport",
   },
 
   {
-    name: 'Annabell Kreiger DVM',
-    email: 'Kamron_Kilback24@gmail.com',
-    phoneNumber: '537.485.6536',
-    image: 'http://lorempixel.com/640/480/sports'
+    name: "Annabell Kreiger DVM",
+    email: "Kamron_Kilback24@gmail.com",
+    phoneNumber: "537.485.6536",
+    image: "http://lorempixel.com/640/480/sports",
   },
 
   {
-    name: 'Sister Langworth',
-    email: 'Mauricio.Predovic@yahoo.com',
-    phoneNumber: '1-728-795-5036 x191',
-    image: 'http://lorempixel.com/640/480/abstract'
+    name: "Sister Langworth",
+    email: "Mauricio.Predovic@yahoo.com",
+    phoneNumber: "1-728-795-5036 x191",
+    image: "http://lorempixel.com/640/480/abstract",
   },
 
   {
-    name: 'Walton Cormier',
-    email: 'Markus25@gmail.com',
-    phoneNumber: '1-218-030-7101',
-    image: 'http://lorempixel.com/640/480/transport'
+    name: "Walton Cormier",
+    email: "Markus25@gmail.com",
+    phoneNumber: "1-218-030-7101",
+    image: "http://lorempixel.com/640/480/transport",
   },
 
   {
-    name: 'Mrs. Jed Anderson',
-    email: 'Shawn.Howell62@hotmail.com',
-    phoneNumber: '1-195-147-1917',
-    image: 'http://lorempixel.com/640/480/cats'
+    name: "Mrs. Jed Anderson",
+    email: "Shawn.Howell62@hotmail.com",
+    phoneNumber: "1-195-147-1917",
+    image: "http://lorempixel.com/640/480/cats",
   },
 
   {
-    name: 'Sam Ledner',
-    email: 'Chase48@hotmail.com',
-    phoneNumber: '701.023.2667 x499',
-    image: 'http://lorempixel.com/640/480/business'
+    name: "Sam Ledner",
+    email: "Chase48@hotmail.com",
+    phoneNumber: "701.023.2667 x499",
+    image: "http://lorempixel.com/640/480/business",
   },
 
   {
-    name: 'Jayson Conroy',
-    email: 'Ayana_Deckow38@gmail.com',
-    phoneNumber: '1-130-094-5404',
-    image: 'http://lorempixel.com/640/480/fashion'
+    name: "Jayson Conroy",
+    email: "Ayana_Deckow38@gmail.com",
+    phoneNumber: "1-130-094-5404",
+    image: "http://lorempixel.com/640/480/fashion",
   },
 
   {
-    name: 'Libbie Kub',
-    email: 'Reymundo_Brekke96@hotmail.com',
-    phoneNumber: '1-632-280-6526',
-    image: 'http://lorempixel.com/640/480/animals'
+    name: "Libbie Kub",
+    email: "Reymundo_Brekke96@hotmail.com",
+    phoneNumber: "1-632-280-6526",
+    image: "http://lorempixel.com/640/480/animals",
   },
 
   {
-    name: 'Tomasa Tillman',
-    email: 'Jordane.Wunsch@yahoo.com',
-    phoneNumber: '1-946-558-9080 x542',
-    image: 'http://lorempixel.com/640/480/business'
+    name: "Tomasa Tillman",
+    email: "Jordane.Wunsch@yahoo.com",
+    phoneNumber: "1-946-558-9080 x542",
+    image: "http://lorempixel.com/640/480/business",
   },
 
   {
-    name: 'Rusty Tremblay MD',
-    email: 'Shana3@yahoo.com',
-    phoneNumber: '773.001.9806',
-    image: 'http://lorempixel.com/640/480/city'
+    name: "Rusty Tremblay MD",
+    email: "Shana3@yahoo.com",
+    phoneNumber: "773.001.9806",
+    image: "http://lorempixel.com/640/480/city",
   },
-
 ];
-
-
